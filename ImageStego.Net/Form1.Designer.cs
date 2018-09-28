3b1312f63ae27c9b2df314b76fdce95a211a4de1
Twist <twistedsprites@gmail.com>
2018-09-28 11:09:06 -0500

    updated ui, created reusable class

--- ImageStego.Net/Form1.Designer.cs
+++ ImageStego.Net/Form1.Designer.cs
@@ -29,58 +29,31 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.btnValidated = new System.Windows.Forms.Button();
             this.tbTextToEncode = new System.Windows.Forms.TextBox();
             this.btnEncodeText = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnDecodeText = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(399, 449);
+            this.pictureBox1.Size = new System.Drawing.Size(504, 413);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(536, 415);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(536, 365);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(149, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Encode Image";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(536, 316);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(149, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Decode Image";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // btnValidated
             // 
-            this.btnValidated.Location = new System.Drawing.Point(417, 22);
+            this.btnValidated.Location = new System.Drawing.Point(6, 390);
             this.btnValidated.Name = "btnValidated";
             this.btnValidated.Size = new System.Drawing.Size(75, 58);
             this.btnValidated.TabIndex = 4;
@@ -89,15 +62,15 @@
             // 
             // tbTextToEncode
             // 
-            this.tbTextToEncode.Location = new System.Drawing.Point(508, 22);
+            this.tbTextToEncode.Location = new System.Drawing.Point(6, 19);
             this.tbTextToEncode.Multiline = true;
             this.tbTextToEncode.Name = "tbTextToEncode";
-            this.tbTextToEncode.Size = new System.Drawing.Size(269, 155);
+            this.tbTextToEncode.Size = new System.Drawing.Size(269, 310);
             this.tbTextToEncode.TabIndex = 5;
             // 
             // btnEncodeText
             // 
-            this.btnEncodeText.Location = new System.Drawing.Point(556, 184);
+            this.btnEncodeText.Location = new System.Drawing.Point(101, 390);
             this.btnEncodeText.Name = "btnEncodeText";
             this.btnEncodeText.Size = new System.Drawing.Size(174, 23);
             this.btnEncodeText.TabIndex = 6;
@@ -105,48 +78,67 @@
             this.btnEncodeText.UseVisualStyleBackColor = true;
             this.btnEncodeText.Click += new System.EventHandler(this.btnEncodeText_Click);
             // 
-            // button4
+            // btnDecodeText
             // 
-            this.button4.Location = new System.Drawing.Point(556, 213);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(174, 23);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "Decode Text";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.btnDecodeText.Location = new System.Drawing.Point(101, 425);
+            this.btnDecodeText.Name = "btnDecodeText";
+            this.btnDecodeText.Size = new System.Drawing.Size(174, 23);
+            this.btnDecodeText.TabIndex = 7;
+            this.btnDecodeText.Text = "Decode Text";
+            this.btnDecodeText.UseVisualStyleBackColor = true;
+            this.btnDecodeText.Click += new System.EventHandler(this.btnDecodeText_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbTextToEncode);
+            this.groupBox1.Controls.Add(this.btnDecodeText);
+            this.groupBox1.Controls.Add(this.btnValidated);
+            this.groupBox1.Controls.Add(this.btnEncodeText);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox1.Location = new System.Drawing.Point(522, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 461);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Encode/Decode Message";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 419);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Open Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.btnEncodeText);
-            this.Controls.Add(this.tbTextToEncode);
-            this.Controls.Add(this.btnValidated);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(809, 461);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Twisted Steno";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button btnValidated;
         private System.Windows.Forms.TextBox tbTextToEncode;
         private System.Windows.Forms.Button btnEncodeText;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnDecodeText;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ ImageStego.Net/ImageStegonography.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageStego.Net
+{
+    public class ImageStegonography
+    {
+        List<ValidatorSet> ValidatorSets = new List<ValidatorSet>();
+        public Bitmap bm = null;
+        private const int yoffset = 50;
+        private const int xoffset = 50;
+        public ImageStegonography(Bitmap image)
+        {
+            this.bm = image;
+            ValidatorSets.Add(new ValidatorSet { Color = Color.FromArgb(255, 192, 192, 192), X = 20, Y = 20 });
+            ValidatorSets.Add(new ValidatorSet { Color = Color.FromArgb(255, 169, 169, 169), X = 20, Y = image.Height - 20 });
+            ValidatorSets.Add(new ValidatorSet { Color = Color.FromArgb(255, 128, 128, 128), X = image.Width - 20, Y = 20 });
+            ValidatorSets.Add(new ValidatorSet { Color = Color.FromArgb(255, 0, 0, 0), X = image.Width - 20, Y = image.Height - 20 });
+        }
+        public string DecodeText()
+        {
+
+            //read data size
+            var dataSize = new List<byte>();
+            var i = 0;
+            var endPixelsDetected = false;
+            while (endPixelsDetected == false)
+            {
+                var sizepixel = bm.GetPixel(i, 30).R;
+                dataSize.Add(sizepixel);
+                if (bm.GetPixel(i + 1, 30).R == 0 && bm.GetPixel(i + 2, 30).R == 0)
+                {
+                    endPixelsDetected = true;
+                }
+                i++;
+            }
+            var totalDataSizeString = Encoding.UTF8.GetString(dataSize.ToArray());
+            var totalDataSize = int.Parse(totalDataSizeString);
+
+            int x = 30;
+            int y = 30;
+            List<byte> bytes = new List<byte>();
+            for (var n = 0; n < totalDataSize; n++)
+            {
+                var ogpixel = bm.GetPixel(x, y);
+                bytes.Add(ogpixel.R);
+                if (x + xoffset < bm.Width) x += xoffset;
+                else
+                {
+                    if (y + yoffset > bm.Height)
+                    {
+                        MessageBox.Show("The message has been truncated.");
+                        break;
+                    }
+                    else y += yoffset;
+                    x = 30;
+                }
+            }
+            var text = Encoding.UTF8.GetString(bytes.ToArray());
+            return text;
+        }
+        public Bitmap EncodeText(string enctext)
+        {
+            bm = CreateSignature();
+
+            var text = Encoding.UTF8.GetBytes(enctext);
+
+            //write data size to prefix
+            var dataSize = Encoding.UTF8.GetBytes(text.Length.ToString());
+            var i = 0;
+            foreach (var bit in dataSize)
+            {
+                var ogpixel = bm.GetPixel(i, 30);
+                bm.SetPixel(i, 30, Color.FromArgb(255, bit, ogpixel.G, ogpixel.B));
+                i++;
+            }
+            //write "end" package so we know the number is over
+            var ogpixels = bm.GetPixel(i, 30);
+            bm.SetPixel(i, 30, Color.FromArgb(255, 0, ogpixels.G, ogpixels.B));
+            bm.SetPixel(i + 1, 30, Color.FromArgb(255, 0, ogpixels.G, ogpixels.B));
+
+
+            int x = 30;
+            int y = 30;
+            foreach (var bit in text)
+            {
+                var ogpixel = bm.GetPixel(x, y);
+                bm.SetPixel(x, y, Color.FromArgb(255, bit, ogpixel.G, ogpixel.B));
+                ogpixel = bm.GetPixel(x, y);
+                if (x + xoffset < bm.Width) x += xoffset;
+                else
+                {
+                    if (y + yoffset > bm.Height)
+                    {
+                        MessageBox.Show("Unable to encode the entire message into the image.");
+                        return null;
+
+                    }
+                    else y += yoffset;
+                    x = 30;
+                }
+
+            }
+            return bm;
+        }
+        public Bitmap CreateSignature()
+        {
+
+            if (bm.Height > 200 && bm.Width > 200)
+            {
+
+                foreach (var item in ValidatorSets)
+                {
+                    bm.SetPixel(item.X, item.Y, item.Color);
+                }
+                if (!IsEncodedImage())
+                {
+                    MessageBox.Show("Message did not encode properly.");
+                    return null;
+                }
+
+                return bm;
+
+            }
+            return null;
+        }
+        public bool IsEncodedImage()
+        {
+            var resultCount = 0;
+
+            if (bm.Height > 200 && bm.Width > 200)
+            {
+
+                foreach (var item in ValidatorSets)
+                {
+                    var pix = bm.GetPixel(item.X, item.Y);
+                    if (pix.A == item.Color.A &&
+                        pix.R == item.Color.R &&
+                        pix.G == item.Color.G &&
+                        pix.B == item.Color.B) resultCount++;
+                    else return false;
+                   
+                }
+
+                if (resultCount == 4)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
