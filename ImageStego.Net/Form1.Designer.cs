a9b864072c2fb146f8a728f1f7003f8541671e59
william harris <twistedsprites@gmail.com>
2018-09-27 23:35:44 -0500

    initial commit

---
+++ ImageStego.Net/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ImageStego.Net
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnValidated = new System.Windows.Forms.Button();
+            this.tbTextToEncode = new System.Windows.Forms.TextBox();
+            this.btnEncodeText = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(399, 449);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(536, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(536, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Encode Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(536, 316);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Decode Image";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnValidated
+            // 
+            this.btnValidated.Location = new System.Drawing.Point(417, 22);
+            this.btnValidated.Name = "btnValidated";
+            this.btnValidated.Size = new System.Drawing.Size(75, 58);
+            this.btnValidated.TabIndex = 4;
+            this.btnValidated.Text = "Validated";
+            this.btnValidated.UseVisualStyleBackColor = true;
+            // 
+            // tbTextToEncode
+            // 
+            this.tbTextToEncode.Location = new System.Drawing.Point(508, 22);
+            this.tbTextToEncode.Multiline = true;
+            this.tbTextToEncode.Name = "tbTextToEncode";
+            this.tbTextToEncode.Size = new System.Drawing.Size(269, 155);
+            this.tbTextToEncode.TabIndex = 5;
+            // 
+            // btnEncodeText
+            // 
+            this.btnEncodeText.Location = new System.Drawing.Point(556, 184);
+            this.btnEncodeText.Name = "btnEncodeText";
+            this.btnEncodeText.Size = new System.Drawing.Size(174, 23);
+            this.btnEncodeText.TabIndex = 6;
+            this.btnEncodeText.Text = "Encode Text";
+            this.btnEncodeText.UseVisualStyleBackColor = true;
+            this.btnEncodeText.Click += new System.EventHandler(this.btnEncodeText_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(556, 213);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(174, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Decode Text";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnEncodeText);
+            this.Controls.Add(this.tbTextToEncode);
+            this.Controls.Add(this.btnValidated);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnValidated;
+        private System.Windows.Forms.TextBox tbTextToEncode;
+        private System.Windows.Forms.Button btnEncodeText;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ImageStego.Net/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageStego.Net
+{
+    public partial class Form1 : Form
+    {
+        private const int NUM_TRIALS = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<ValidatorSet> ValidatorSets = new List<ValidatorSet>();
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            pictureBox1.ImageLocation = @"C:\Users\Twist\Pictures\Camera Roll\20180922_113337_MixedReality.jpg";
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            //Try a small pic to be able to compare output, 
+            //a big one to compare performance
+
+        }
+        public void doSomethingWithBitmapFast(System.Drawing.Bitmap bmp)
+        {
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            Bitmap bm = new Bitmap(pictureBox1.Image);
+            for (int trial = 0; trial < NUM_TRIALS; trial++)
+            {
+                for (int Y = 0; Y < bm.Height; Y++)
+                {
+                    for (int X = 0; X < bm.Width; X++)
+                    {
+                        Color clr = bm.GetPixel(X, Y);
+                        clr = Color.FromArgb(255 - clr.R, 255 - clr.G, 255 - clr.B);
+                        bm.SetPixel(X, Y, Color.FromArgb(255,clr.R,clr.G, clr.B));
+                    }
+                }
+            }
+            pictureBox1.Image = bm;
+            watch.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Bitmap b = (Bitmap)pictureBox1.Image;
+            doSomethingWithBitmapFast(b);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(pictureBox1.Image);
+
+            ValidatorSets.Clear();
+
+            ValidatorSets.Add(new ValidatorSet { Color = Color.FromArgb(255, 255, 0, 0), X = 20, Y = 20 });
+            ValidatorSets.Add(new ValidatorSet { Color = Color.FromArgb(255, 0, 255, 0), X = 20, Y = bm.Height - 20 });
+
+            ValidatorSets.Add(new ValidatorSet { Color = Color.FromArgb(255, 0, 255, 0), X = bm.Width - 20, Y = 20 });
+            ValidatorSets.Add(new ValidatorSet { Color = Color.FromArgb(255, 0, 0, 0), X = bm.Width - 20, Y = bm.Height - 20 });
+
+            if (bm.Height > 200 && bm.Width > 200)
+            {
+                var clr = Color.FromArgb(255, 255, 255);
+                //var item = bm.GetPixel(20, 20);
+
+                foreach (var item in ValidatorSets)
+                {
+                    bm.SetPixel(item.X, item.Y, item.Color);
+                }
+
+                pictureBox1.Image = bm;
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(pictureBox1.Image);
+
+            ValidatorSets.Clear();
+
+            ValidatorSets.Add(new ValidatorSet { Color = Color.FromArgb(255, 255, 0, 0), X = 20, Y = 20 });
+            ValidatorSets.Add(new ValidatorSet { Color = Color.FromArgb(255, 0, 255, 0), X = 20, Y = bm.Height - 20 });
+
+            ValidatorSets.Add(new ValidatorSet { Color = Color.FromArgb(255, 0, 255, 0), X = bm.Width - 20, Y = 20 });
+            ValidatorSets.Add(new ValidatorSet { Color = Color.FromArgb(255, 0, 0, 0), X = bm.Width - 20, Y = bm.Height - 20 });
+
+            var resultCount = 0;
+            
+            if (bm.Height > 200 && bm.Width > 200)
+            {
+                var clr = Color.FromArgb(255, 255, 255);
+                //var item = bm.GetPixel(20, 20);
+                
+                foreach (var item in ValidatorSets)
+                {
+                    var pix = bm.GetPixel(item.X, item.Y);
+                    if (pix.A == item.Color.A &&
+                        pix.R == item.Color.R &&
+                        pix.G == item.Color.G &&
+                        pix.B == item.Color.B) resultCount++;
+
+                }
+
+                if (resultCount == 4)
+                {
+                    btnValidated.BackColor = Color.Green;
+                } else
+                {
+                    btnValidated.BackColor = Color.Red;
+                }
+
+                pictureBox1.Image = bm;
+
+            }
+        }
+
+        private void btnEncodeText_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(pictureBox1.Image);
+
+            var text = Encoding.UTF8.GetBytes(tbTextToEncode.Text);
+
+            int x = 30;
+            int y = 30;
+            foreach (var bit in text)
+            {
+                var ogpixel = bm.GetPixel(x, y);
+                bm.SetPixel(x, y, Color.FromArgb(255, bit, ogpixel.G, ogpixel.B ));
+                ogpixel = bm.GetPixel(x, y);
+                if (x < bm.Width) x += 10;
+                else
+                {
+                    if (y + 10 > bm.Height)
+                    {
+                        MessageBox.Show("Unable to encode the entire message into the image.");
+                        return;
+
+                    }
+                    else y += 10;
+                    x = 30;
+                }
+
+            }
+            pictureBox1.Image = bm;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(pictureBox1.Image);
+
+  
+
+            int x = 30;
+            int y = 30;
+            List<byte> bytes = new List<byte>();
+            for (var i= 0; i < 9; i++)
+            {
+                var ogpixel = bm.GetPixel(x, y);
+                bytes.Add(ogpixel.R);
+                if (x < bm.Width) x += 10;
+                else
+                {
+                    if (y + 10 > bm.Height)
+                    {
+                        MessageBox.Show("Unable to encode the entire message into the image.");
+                        return;
+
+                    }
+                    else y += 10;
+                    x = 30;
+                }
+
+            }
+            var text = Encoding.UTF8.GetString(bytes.ToArray());
+            tbTextToEncode.Text = text;
+        }
+    }
+
+}
+
